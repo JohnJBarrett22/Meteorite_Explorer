04632de7beadf4dfccb092a153c47f50c6f61587
John Barrett <johnjbarrett22@gmail.com>
2019-05-18 22:51:27 -0700

    Linked out to NASA API, added Bootstrap, added table, modified styling some

--- apps/meteorite_app/views.py
+++ apps/meteorite_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
+import requests
 
 # Create your views here.
 def index(request):
+    url = 'https://data.nasa.gov/resource/gh4g-9sfh.json'
+    r = requests.get(url)
+    print(r.text.encode("utf-8"))
+
     return render(request, "meteorite_app/index.html")
