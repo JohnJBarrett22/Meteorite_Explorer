b7cf91083f8570a9829e64689d16a007e0ed9831
John Barrett <johnjbarrett22@gmail.com>
2019-05-19 01:04:16 -0700

    API in place for full data pull, working on time format and need to add form for filtering

--- apps/meteorite_app/views.py
+++ apps/meteorite_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 import requests
 
-# Create your views here.
+
 def index(request):
     url = 'https://data.nasa.gov/resource/gh4g-9sfh.json'
-    r = requests.get(url)
-    print(r.text.encode("utf-8"))
+    r = requests.get(url).json()
 
-    return render(request, "meteorite_app/index.html")
+    context = {'meteorites_data' : r}
+
+    return render(request, "meteorite_app/index.html", context)
