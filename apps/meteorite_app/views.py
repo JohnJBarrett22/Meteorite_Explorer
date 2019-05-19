20d38e1a432a0450483d5218074c38b75dd23781
John Barrett <johnjbarrett22@gmail.com>
2019-05-19 03:07:22 -0700

    Corrected search function to include query refinement, altered search.html

--- apps/meteorite_app/views.py
+++ apps/meteorite_app/views.py
@@ -10,3 +10,11 @@ def index(request):
 
     return render(request, "meteorite_app/index.html", context)
 
+def search(request):
+    name = request.POST['name']
+    url = 'https://data.nasa.gov/resource/gh4g-9sfh.json?name={}'.format(name)
+    r = requests.get(url).json()
+
+    context = {'meteorites_data' : r}
+
+    return render(request, "meteorite_app/search.html", context)
