e320ddc54aa7be5744b9f038ebc55cead7d9525c
John Barrett <johnjbarrett22@gmail.com>
2019-05-19 03:50:19 -0700

    Layered in if check for search function to meet reset requirement

--- apps/meteorite_app/views.py
+++ apps/meteorite_app/views.py
@@ -5,16 +5,21 @@ import requests
 def index(request):
     url = 'https://data.nasa.gov/resource/gh4g-9sfh.json'
     r = requests.get(url).json()
-
     context = {'meteorites_data' : r}
 
     return render(request, "meteorite_app/index.html", context)
 
 def search(request):
     name = request.POST['name']
+
+    if name == '':
+        url = 'https://data.nasa.gov/resource/gh4g-9sfh.json'
+        r = requests.get(url).json()
+        context = {'meteorites_data' : r}
+        return render(request, "meteorite_app/index.html", context)
+    
     url = 'https://data.nasa.gov/resource/gh4g-9sfh.json?name={}'.format(name)
     r = requests.get(url).json()
-
     context = {'meteorites_data' : r}
 
     return render(request, "meteorite_app/search.html", context)
