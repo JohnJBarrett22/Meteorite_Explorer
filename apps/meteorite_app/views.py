d47303220c398344dabcf3f9aaebf796b9834c8c
John Barrett <johnjbarrett22@gmail.com>
2019-05-19 23:28:57 -0700

    Corrected query...again!

--- apps/meteorite_app/views.py
+++ apps/meteorite_app/views.py
@@ -11,8 +11,6 @@ def index(request):
 
 def search(request):
     name = request.POST['name']
-    name = name.lower()
-    name = name.title()
     print(name)
 
     if name == '':
@@ -21,7 +19,7 @@ def search(request):
         context = {'meteorites_data' : r}
         return render(request, "meteorite_app/index.html", context)
     
-    url = 'https://data.nasa.gov/resource/gh4g-9sfh.json?$where=lower(name)%20like%20lower(%22%25Aa%25%22)'
+    url = 'https://data.nasa.gov/resource/gh4g-9sfh.json?$where=lower(name)%20like%20lower(%22%25{}%25%22)'.format(name)
     r = requests.get(url).json()
     context = {'meteorites_data' : r}
 
